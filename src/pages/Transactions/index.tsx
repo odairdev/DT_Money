ef741b0f0cc6b4250236ff913714c9c01ac02e50
Odair José Custodio Junior <odair.dev@gmail.com>
2022-08-29 12:04:37 -0300

    Transactions Table added

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,34 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 
 export function Transactions() {
   return (
-    <main>
+    <div>
       <Header />
       <Summary />
-    </main>
+
+      <TransactionsContainer>
+      <TransactionsTable>
+        <tbody>
+          <tr>
+            <td width='50%'>Desenvolvimento de site</td>
+            <td><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
+            <td>Venda</td>
+            <td>13/04/2022</td>
+          </tr>
+          <tr>
+            <td width='50%'>Hamburger</td>
+            <td><PriceHighlight variant="outcome">- R$ 59,00</PriceHighlight></td>
+            <td>Alimentação</td>
+            <td>18/04/2022</td>
+          </tr>
+        </tbody>
+      </TransactionsTable>
+      </TransactionsContainer>
+
+    </div>
 
   )
 }
