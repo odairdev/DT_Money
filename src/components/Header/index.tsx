56be62c276e59cb537bbf98974dbd67eee2827a3
Odair José Custodio Junior <odair.dev@gmail.com>
2022-08-30 09:06:25 -0300

    Layout finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import Logo from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -11,7 +13,13 @@ export function Header() {
           <h1>DT Money</h1>
         </div>
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
---
+++ src/components/NewTransactionModal/styles.ts
@@ -0,0 +1,117 @@
+import styled from "styled-components";
+import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from "@radix-ui/react-radio-group";
+
+export const Overlay = styled(Dialog.Overlay)`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  inset: 0;
+
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+export const Content = styled(Dialog.Content)`
+  min-width: 32rem;
+  border-radius: 6px;
+  padding: 2.5rem 3rem;
+  background-color: ${(props) => props.theme["gray-800"]};
+
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  form {
+    margin-top: 2rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    input {
+      border: 0;
+      border-radius: 6px;
+      padding: 1rem;
+      background-color: ${(props) => props.theme["gray-900"]};
+      color: ${(props) => props.theme["gray-300"]};
+
+      &::placeholder {
+        color: ${(props) => props.theme["gray-500"]};
+      }
+    }
+
+    button[type="submit"] {
+      border: 0;
+      cursor: pointer;
+      border-radius: 6px;
+      padding: 0 1.25rem;
+      height: 58px;
+      width: 100%;
+      background-color: ${(props) => props.theme["green-500"]};
+      font-weight: bold;
+      margin-top: 1.5rem;
+
+      &:hover {
+        background-color: ${(props) => props.theme["green-700"]};
+        transition: 0.2s;
+      }
+    }
+  }
+`;
+
+export const CloseButton = styled(Dialog.Close)`
+  background-color: transparent;
+  border: 0;
+  color: ${(props) => props.theme["gray-500"]};
+  position: absolute;
+  top: 1.5rem;
+  right: 1.5rem;
+  line-height: 0;
+  cursor: pointer;
+
+  &:hover {
+    color: ${(props) => props.theme["gray-300"]};
+  }
+`;
+
+export const TransactionType = styled(RadioGroup.Root)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 1.5rem;
+`
+
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome';
+}
+
+export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButtonProps>`
+  background-color: ${props => props.theme["gray-700"]};
+  padding: 1rem 1.5rem;
+  border-radius: 6px;
+  border: 0;
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+  justify-content: center;
+  color: ${props => props.theme["gray-300"]};
+  cursor: pointer;
+
+  svg {
+      color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]}
+    }
+
+    &[data-state="unchecked"]:hover {
+      background-color: ${props => props.theme["gray-600"]};
+    }
+
+  &[data-state="checked"] {
+    background-color: ${props => props.variant === 'income' ? props.theme["green-500"] : props.theme["red-500"]};
+    color: ${props => props.theme.white};
+
+    svg {
+      color: ${props => props.theme.white};
+    }
+  }
+`
